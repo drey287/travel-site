87b55e520dfe6616a3e46c5a52f94a0a3f0621a2
Andrei Ciubotariu <andrei.ciubotariu8@gmail.com>
2018-01-17 10:20:23 +0200

    changes done yestorday

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer');
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import');
+	
 
 gulp.task('default', function () {
 	console.log('felicitari ai trecut de manageia gulp ');
@@ -13,7 +17,7 @@ gulp.task('html', function () {
 
 gulp.task('styles', function () {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([autoprefixer]))
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
